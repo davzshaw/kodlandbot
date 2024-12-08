f234df052c4bce51c60e2d47d93bfd14b1b21d64
David Torres <119999924+davzshaw@users.noreply.github.com>
2024-12-08 15:40:27 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,36 @@
 import discord
+import os
+import random
 from discord.ext import commands
-from botlogic import generatePassword
-from datetime import datetime
-
-t = "TOKEN HERE"
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!kodlandbot ', intents=intents)
+bot = commands.Bot(command_prefix='!k ', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 @bot.command()
-async def hello(ctx):
-    await ctx.send(f'Hola, soy un bot {bot.user}!')
+async def m1(ctx):
+    with open('c5/img/m1.jpg', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
 
 @bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
+async def showImg(ctx):
+    listaImg = os.listdir('c5/img')
+    await ctx.send(listaImg)
     
-@bot.command()
-async def password(ctx, passwordLenght):
-    passwordLenght = int(passwordLenght)
-    await ctx.send(generatePassword(passwordLenght))
+    
 
-bot.run(t)
+@bot.command()
+async def animal(ctx):
+    with open('c5/animal/m1.jpg', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+from data import TOKEN
+bot.run(token=TOKEN)
+
